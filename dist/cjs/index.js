86989082f1572c892c819dacb2b0202428b17481
Kevin Malakoff <kmalakoff@gmail.com>
2022-07-23 18:41:39 -0700

    Upgrade modules

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -2,17 +2,30 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.EventProvider = EventProvider;
-exports.useEvent = useEvent;
-exports.EventContext = void 0;
-var _react = _interopRequireDefault(require("react"));
+function _export(target, all) {
+    for(var name in all)Object.defineProperty(target, name, {
+        enumerable: true,
+        get: all[name]
+    });
+}
+_export(exports, {
+    EventContext: function() {
+        return EventContext;
+    },
+    EventProvider: function() {
+        return EventProvider;
+    },
+    useEvent: function() {
+        return useEvent;
+    }
+});
+var _react = /*#__PURE__*/ _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) {
     return obj && obj.__esModule ? obj : {
         default: obj
     };
 }
 var EventContext = /*#__PURE__*/ _react.default.createContext(undefined);
-exports.EventContext = EventContext;
 function EventProvider(param) {
     var _events = param.events, events = _events === void 0 ? [
         "click"
@@ -57,4 +70,10 @@ function useEvent(handler, dependencies) {
         context.subscribe,
         handler
     ].concat(dependencies));
+}
+
+if ((typeof exports.default === 'function' || (typeof exports.default === 'object' && exports.default !== null)) && typeof exports.default.__esModule === 'undefined') {
+  Object.defineProperty(exports.default, '__esModule', { value: true });
+  for (var key in exports) exports.default[key] = exports[key];
+  module.exports = exports.default;
 }
