4cc49d14b2985ead4a52392fb85215678173a583
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 22:21:15 -0800

    Migrate to Biome and update build configuration

--- test/exports/unpkg.min.test.cjs
+++ test/exports/unpkg.min.test.cjs
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const assert = require('assert');
-const { EventContext, useEvent, EventProvider } = require('react-dom-event');
+const { EventContext, useEvent, EventProvider } = require('react-dom-event/dist/umd/react-dom-event.min.cjs');
 
-describe('exports react-dom-event/dist/umd/react-dom-event.min.js', function () {
-  it('defaults', function () {
+describe('exports react-dom-event/dist/umd/react-dom-event.min.cjs', () => {
+  it('defaults', () => {
     assert.equal(typeof EventContext, 'object');
     assert.equal(typeof EventProvider, 'function');
     assert.equal(typeof useEvent, 'function');
--- test/exports/unpkg.test.cjs
+++ test/exports/unpkg.test.cjs
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const assert = require('assert');
-const { EventContext, useEvent, EventProvider } = require('react-dom-event');
+const { EventContext, useEvent, EventProvider } = require('react-dom-event/dist/umd/react-dom-event.cjs');
 
-describe('exports react-dom-event/dist/umd/react-dom-event.js', function () {
-  it('defaults', function () {
+describe('exports react-dom-event/dist/umd/react-dom-event.cjs', () => {
+  it('defaults', () => {
     assert.equal(typeof EventContext, 'object');
     assert.equal(typeof EventProvider, 'function');
     assert.equal(typeof useEvent, 'function');
