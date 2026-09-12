kmalakoff/react-dom-outside

Update package exports and type definitions

test/exports/umd.test.cjs
@@ -1,7 +1,15 @@
 const assert = require('assert');
-const { Active, ActiveBoundary } = require('react-dom-outside/dist/umd/react-dom-outside.cjs');
 
-describe('exports react-dom-outside/dist/umd/react-dom-outside.cjs', () => {
+let umd = null;
+try {
+  umd = require('react-dom-outside/umd');
+} catch (_) {
+  umd = require('react-dom-outside/dist/umd/react-dom-outside.cjs');
+}
+const reactDomOutside = typeof window !== 'undefined' ? window.reactDomOutside : umd.default || umd;
+const { Active, ActiveBoundary } = reactDomOutside;
+
+describe('exports umd', () => {
   it('defaults', () => {
     assert.equal(typeof Active, 'function');
     assert.equal(typeof ActiveBoundary, 'function');

kmalakoff/react-dom-event

test/exports/umd.test.cjs
@@ -1,7 +1,15 @@
 const assert = require('assert');
-const { EventContext, useEvent, EventProvider } = require('react-dom-event/dist/umd/react-dom-event.cjs');
 
-describe('exports react-dom-event/dist/umd/react-dom-event.cjs', () => {
+let umd = null;
+try {
+  umd = require('react-dom-event/umd');
+} catch (_) {
+  umd = require('react-dom-event/dist/umd/react-dom-event.cjs');
+}
+const reactDomEvent = typeof window !== 'undefined' ? window.reactDomEvent : umd.default || umd;
+const { EventContext, useEvent, EventProvider } = reactDomEvent;
+
+describe('exports umd', () => {
   it('defaults', () => {
     assert.equal(typeof EventContext, 'object');
     assert.equal(typeof EventProvider, 'function');